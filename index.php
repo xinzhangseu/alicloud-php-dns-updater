8a7789328358aa36f425e03adcfee82407d8d47e
Xin Zhang <x.zhang.seu@gmail.com>
2023-02-11 20:59:57 +0800

    aliyun

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ include_once 'alicloud-php-updaterecord/V20150109/AlicloudUpdateRecord.php';
 
 use Roura\Alicloud\V20150109\AlicloudUpdateRecord;
 
-$AccessKeyId     = 'ACCESS_KEY_ID';
-$AccessKeySecret = 'ACCESS_KEY_SECRET';
+$AccessKeyId     = 'LTAI5tRKV9fRZ2BHG4mq2Wqt';
+$AccessKeySecret = 'SVAXOvHLWy7LrR0ltpiSCl1RyvM1Zq';
 $updater         = new AlicloudUpdateRecord($AccessKeyId, $AccessKeySecret);
 
 $newIp = $_SERVER['REMOTE_ADDR']; // New IP
 
-$updater->setDomainName('DOMAIN.COM');
+$updater->setDomainName('xzhang.ink');
 $updater->setRecordType('A');
 $updater->setRR('@');
 $updater->setValue($newIp);
